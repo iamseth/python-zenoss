efc21987ded991cb9211dd2b048deb3cb1594c1b
Seth Miller <seth@sethmiller.me>
2015-07-30 11:28:21 -0500

    increment version to 0.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='zenoss',
-    version='0.5.1',
+    version='0.6.0',
     description='Module to work with the Zenoss JSON API.',
     author="Seth Miller",
     author_email='seth@seth-miller.com',
