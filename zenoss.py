75036fe778fb45909d7374ca84a1f76886852e1c
Seth Miller <seth@sethmiller.me>
2015-07-30 11:13:11 -0500

    replace double quotes with single

--- zenoss.py
+++ zenoss.py
@@ -80,17 +80,17 @@ class Zenoss(object):
         return ast.literal_eval(self.__session.get(url, params=params).content)
 
     def get_devices(self, device_class='/zport/dmd/Devices', limit=None):
-        """Get a list of all devices.
+        '''Get a list of all devices.
 
-        """
+        '''
         log.info('Getting all devices')
         return self.__router_request('DeviceRouter', 'getDevices',
                                      data=[{'uid': device_class, 'params': {}, 'limit': limit}])
 
     def find_device(self, device_name):
-        """Find a device by name.
+        '''Find a device by name.
 
-        """
+        '''
         log.info('Finding device %s', device_name)
         all_devices = self.get_devices()
 
@@ -108,56 +108,56 @@ class Zenoss(object):
         return self.find_device(device)['uid']
 
     def add_device(self, device_name, device_class, collector='localhost'):
-        """Add a device.
+        '''Add a device.
 
-        """
+        '''
         log.info('Adding %s', device_name)
         data = dict(deviceName=device_name, deviceClass=device_class, model=True, collector=collector)
         return self.__router_request('DeviceRouter', 'addDevice', [data])
 
     def remove_device(self, device_name):
-        """Remove a device.
+        '''Remove a device.
 
-        """
+        '''
         log.info('Removing %s', device_name)
         device = self.find_device(device_name)
         data = dict(uids=[device['uid']], hashcheck=device['hash'], action='delete')
         return self.__router_request('DeviceRouter', 'removeDevices', [data])
 
     def move_device(self, device_name, organizer):
-        """Move the device the organizer specified.
+        '''Move the device the organizer specified.
 
-        """
+        '''
         log.info('Moving %s to %s', device_name, organizer)
         device = self.find_device(device_name)
         data = dict(uids=[device['uid']], hashcheck=device['hash'], target=organizer)
         return self.__router_request('DeviceRouter', 'moveDevices', [data])
 
     def set_prod_state(self, device_name, prod_state):
-        """Set the production state of a device.
+        '''Set the production state of a device.
 
-        """
+        '''
         log.info('Setting prodState on %s to %s', device_name, prod_state)
         device = self.find_device(device_name)
         data = dict(uids=[device['uid']], prodState=prod_state, hashcheck=device['hash'])
         return self.__router_request('DeviceRouter', 'setProductionState', [data])
 
     def set_maintenance(self, device_name):
-        """Helper method to set prodState for device so that it does not alert.
+        '''Helper method to set prodState for device so that it does not alert.
 
-        """
+        '''
         return self.set_prod_state(device_name, 300)
 
     def set_production(self, device_name):
-        """Helper method to set prodState for device so that it is back in production and alerting.
+        '''Helper method to set prodState for device so that it is back in production and alerting.
 
-        """
+        '''
         return self.set_prod_state(device_name, 1000)
 
     def set_product_info(self, device_name, hw_manufacturer, hw_product_name, os_manufacturer, os_product_name): # pylint: disable=R0913
-        """Set ProductInfo on a device.
+        '''Set ProductInfo on a device.
 
-        """
+        '''
         log.info('Setting ProductInfo on %s', device_name)
         device = self.find_device(device_name)
         data = dict(uid=device['uid'],
@@ -168,7 +168,7 @@ class Zenoss(object):
         return self.__router_request('DeviceRouter', 'setProductInfo', [data])
 
     def set_rhel_release(self, device_name, release):
-        """Sets the proper release of RedHat Enterprise Linux."""
+        '''Sets the proper release of RedHat Enterprise Linux.'''
         if type(release) is not float:
             log.error("RHEL release must be a float")
             return {u'success': False}
@@ -178,46 +178,46 @@ class Zenoss(object):
                                      'RHEL {}'.format(release))
 
     def set_device_info(self, device_name, data):
-        """Set attributes on a device or device organizer.
+        '''Set attributes on a device or device organizer.
             This method accepts any keyword argument for the property that you wish to set.
 
-        """
+        '''
         data['uid'] = self.find_device(device_name)['uid']
         return self.__router_request('DeviceRouter', 'setInfo', [data])
 
     def remodel_device(self, device_name):
-        """Submit a job to have a device remodeled.
+        '''Submit a job to have a device remodeled.
 
-        """
+        '''
         return self.__router_request('DeviceRouter', 'remodel', [dict(uid=self.find_device(device_name)['uid'])])
 
     def set_collector(self, device_name, collector):
-        """Set collector for device.
+        '''Set collector for device.
 
-        """
+        '''
         device = self.find_device(device_name)
         data = dict(uids=[device['uid']], hashcheck=device['hash'], collector=collector)
         return self.__router_request('DeviceRouter', 'setCollector', [data])
 
     def rename_device(self, device_name, new_name):
-        """Rename a device.
+        '''Rename a device.
 
-        """
+        '''
         data = dict(uid=self.find_device(device_name)['uid'], newId=new_name)
         return self.__router_request('DeviceRouter', 'renameDevice', [data])
 
     def reset_ip(self, device_name, ip_address=''):
-        """Reset IP address(es) of device to the results of a DNS lookup or a manually set address.
+        '''Reset IP address(es) of device to the results of a DNS lookup or a manually set address.
 
-        """
+        '''
         device = self.find_device(device_name)
         data = dict(uids=[device['uid']], hashcheck=device['hash'], ip=ip_address)
         return self.__router_request('DeviceRouter', 'resetIp', [data])
 
     def get_events(self, device=None, limit=100, component=None, event_class=None):
-        """Find current events.
+        '''Find current events.
 
-        """
+        '''
         data = dict(start=0, limit=limit, dir='DESC', sort='severity')
         data['params'] = dict(severity=[5, 4, 3, 2], eventState=[0, 1])
         if device:
@@ -230,42 +230,42 @@ class Zenoss(object):
         return self.__router_request('EventsRouter', 'query', [data])['events']
 
     def get_event_detail(self, event_id):
-        """Find specific event details
+        '''Find specific event details
 
-        """
+        '''
         data = dict(evid=event_id)
         return self.__router_request('EventsRouter', 'detail', [data])
 
     def write_log(self, event_id, message):
-        """Write a message to the event's log
+        '''Write a message to the event's log
 
-        """
+        '''
         data = dict(evid=event_id, message=message)
         return self.__router_request('EventsRouter', 'write_log', [data])
 
     def change_event_state(self, event_id, state):
-        """Change the state of an event.
+        '''Change the state of an event.
 
-        """
+        '''
         log.info('Changing eventState on %s to %s', event_id, state)
         return self.__router_request('EventsRouter', state, [{'evids': [event_id]}])
 
     def ack_event(self, event_id):
-        """Helper method to set the event state to acknowledged.
+        '''Helper method to set the event state to acknowledged.
 
-        """
+        '''
         return self.change_event_state(event_id, 'acknowledge')
 
     def close_event(self, event_id):
-        """Helper method to set the event state to closed.
+        '''Helper method to set the event state to closed.
 
-        """
+        '''
         return self.change_event_state(event_id, 'close')
 
     def create_event_on_device(self, device_name, severity, summary):
-        """Manually create a new event for the device specified.
+        '''Manually create a new event for the device specified.
 
-        """
+        '''
         log.info('Creating new event for %s with severity %s', device_name, severity)
         if severity not in ('Critical', 'Error', 'Warning', 'Info', 'Debug', 'Clear'):
             raise Exception('Severity %s is not valid.' % severity)
@@ -273,8 +273,8 @@ class Zenoss(object):
         return self.__router_request('EventsRouter', 'add_event', [data])
 
     def get_load_average(self, device):
-        """Returns the current 1, 5 and 15 minute load averages for a device.
-        """
+        '''Returns the current 1, 5 and 15 minute load averages for a device.
+        '''
         result = self.get_rrd_values(device=device, dsnames=['laLoadInt1_laLoadInt1', 'laLoadInt5_laLoadInt5', 'laLoadInt15_laLoadInt15'])
         def normalize_load(l):
             return round(float(l) / 100.0, 2)
